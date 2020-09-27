a7e5e6ba127db4b8c073cdfcd18d4c70a28d7073
Luke Balaj <Ldb222@lehigh.edu>
2020-09-27 14:08:59 -0400

    Assignment 2

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,10 +1,22 @@
-library(dplyr)
+library("tidyverse")
 #set working directory
-#setwd(./"covid-19-data/")
-US <- read.csv("covid-19-data/us-states.csv")
+setwd("~/Documents/BIS244/BIS-244-balajluke/COVID-19_Project/covid-19-data")
+US <- read_csv("us-states.csv")
 
 #filter for just PA
-PA_data <- states %>% filter(state == "Pennsylvania")
+pa_data <- filter(US, state == "Pennsylvania")
+pa_data <- mutate(pa_data, adj_deaths = deaths)
+n <- length(pa_data$date)
 
-#create an adjustment
-adjusted <- PA_data %>% mutate(adj_deaths = deaths)
+for (i in 1:n){
+  if (pa_data$date[i]=="2020-04-21"){
+    pa_data$adj_deaths[i] <- pa_data$deaths[i]-282}
+}
+
+for (i in 1:n){
+  if (pa_data$date[i]=="2020-04-22"){
+    pa_data$adj_deaths[i] <- pa_data$deaths[i]-297}
+}
+
+#sum of all adj_deaths in PA
+sum(pa_data$adj_deaths)
